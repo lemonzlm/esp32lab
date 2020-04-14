8fa41fe2f78357b4a6120d7e766cea74b4640bb5
lemonzlm <zlm.gd.cn@gmail.com>
2020-04-14 21:32:49 +0800

    update

--- _book/code/wifiAp 5.py
+++
@@ -1,8 +0,0 @@
-import network
-def createAp():
-    ap = network.WLAN(network.AP_IF)
-    ap.active(True)
-    ap.config(essid='espap-lemon') # d当后面增加audomod 参数时会出错。
-    ap.status()
-    ap.__dir__()
-
